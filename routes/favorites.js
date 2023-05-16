ed5242f3e5aea3a687d4d04b917d998791865dcc
AudreyAAOO <audrey.s.carpentier@gmail.com>
2023-05-16 14:11:17 +0200

    ajout logs

--- routes/favorites.js
+++ routes/favorites.js
@@ -10,34 +10,27 @@ router.post("/favorites", async (req, res) => {
     try {
         console.log("route favorites ok ?");
         console.log("req.body: ", req.body);
-        const { arrayOfId } = req.body;
+
+        // const arrayOfId = req.body.pinsChar;
+        const { pinsChar } = req.body;
+        console.log("--★--★--★--★ arrayOfId/pinsChar: ", pinsChar);
+
         const arrayChar = [];
 
-        for (let i = 0; i < arrayOfId.length; i++) {
-            const characterId = arrayOfId[i];
-            // console.log("characterId = arrayChar[i]: ", characterId);
+        for (let i = 0; i < pinsChar.length; i++) {
+            const characterId = pinsChar[i];
 
-            // arrayChar.forEach(resultId => {
             const response = await axios.get(
                 `https://lereacteur-marvel-api.herokuapp.com/character/${characterId}?apiKey=${process.env.API_KEY_MARVEL}`
             )
-            //    console.log("response: ", response);
-            // console.log("response.data : ", response.data);
-
-            // })
-
+            console.log("response.data : ", response.data);
             arrayChar.push(response.data);
-            console.log("arrayChar: ", arrayChar);
-
-            // for (let i = 0; i < response.data.length; i++) {
-            //     
-            // }
         }
         res.status(200).json(arrayChar);
-        console.log("--★--★--★--★ arrayChar: --★--★--★--★--★", arrayChar);
+        // console.log("--★--★--★--★ arrayChar: --★--★--★--★--★", arrayChar);
     } catch (error) {
         res.status(400).json({ message: error.message });
-        console.log("BACK error.response: ", error.message);
+        console.log("error: ", error);
     }
 });
 
